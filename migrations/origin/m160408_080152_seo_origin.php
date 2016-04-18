a1a3f5b22b0116f1b036eba88dd1908dd80c71d2
eugene <compuniti@mail.ru>
2016-04-18 14:40:56 +0300

    Исправление заусенцев

--- migrations/origin/m160408_080152_seo_origin.php
+++ migrations/origin/m160408_080152_seo_origin.php
@@ -9,14 +9,10 @@ class m160408_080152_seo_origin extends Migration
     public function safeUp()
     {
         $this->addColumn('seo_text', 'origin_id', \yii\db\Schema::TYPE_INTEGER.'(11)');
-        $this->dropPrimaryKey('pk_seo_text_url_position', 'seo_text');
-        $this->addPrimaryKey('pk_seo_text_url_position_origin_id', 'seo_text', ['url', 'position', 'origin_id']);
     }
 
     public function safeDown()
     {
-        $this->dropPrimaryKey('pk_seo_text_url_position_origin_id', 'seo_text');
-        $this->addPrimaryKey('pk_seo_text_url_position', 'seo_text', ['url', 'position']);
         $this->dropColumn('seo_text', 'origin_id');
     }
 }
